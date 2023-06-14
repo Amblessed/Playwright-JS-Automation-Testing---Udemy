a5f97130b4b8c9bfc34137b0d5991031e10a7a5c
Okechukwu Bright Onwumere <brighthamza9@yahoo.com>
2023-06-14 18:38:00 +0200

    Implemented more test

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -7,11 +7,12 @@ test("Print Multiple Elements Text Test", async ({page}) => {
     const username = page.locator("input#userEmail");
     const password = page.locator("input#userPassword");
     const signIn = page.locator("input#login");
-    const cardTitles = page.locator("div.card-body > h5 > b");
+    const email = "anshika@gmail.com";
+
     const products = page.locator("div.card-body");
     const productName = "zara coat 3";
 
-    await username.type("anshika@gmail.com");
+    await username.type(email);
     await password.type("Iamking@000");
     await signIn.click();
 
@@ -45,8 +46,55 @@ test("Print Multiple Elements Text Test", async ({page}) => {
             break;
         }
     }
+
+    await page.locator("ul [type='button'][class='btn btn-primary']").click();
+
+    await page.locator("div[class='field small'] input[class='input txt']").type("475");
+    await page.locator("div[class='field'] input[class='input txt']").type("John");
+    await page.locator("[placeholder='Select Country']").type("ger", {delay:100});
+    const dropdown = page.locator("[class*='ta-results']")
+    await dropdown.waitFor();
+    const dropdownCount = await dropdown.locator("button").count();
+
+    for(let i = 0; i < dropdownCount; i++)
+    {
+        const text = await dropdown.locator("button").nth(i).textContent();
+        if(text === " Germany")
+        {
+            await dropdown.locator("button").nth(i).click();
+            break;
+        }
+    }
+
+    const emailEntered = await page.locator("[class*='user__name'] label[type='text']").textContent();
+    expect(emailEntered).toEqual(email);
     
-    
-    //await page.close();
+
+    await page.locator("[class*='action__submit']").click();
+
+    await expect(page.locator("[class*='hero-primary']")).toHaveText(" Thankyou for the order. ");
+
+    var orderId = await page.locator("[class='em-spacer-1'] [class='ng-star-inserted']").textContent();
+    orderId = orderId.replaceAll("|", "").trim()
+
+    await page.locator("td [routerlink='/dashboard/myorders']").click();
+
+    //Get all the orderId of the table displayed
+    await page.waitForSelector("tr")
+    const orders = page.locator("tr");
+    const ordersCount = await orders.count();
+    //const orders = await page.$$("tr");
+
+    for(let i = 0; i < ordersCount; i++)
+    {
+        const orderIDText = await orders.locator("th[scope='row']").nth(i).textContent();
+        if(orderIDText === orderId){
+            await orders.locator("button[class*='btn-primary']").nth(i).click();
+            break;
+        }
+    }
+
+    const orderIdSummaryPage = await page.locator("small[class='col-title'] + div").textContent();
+    expect(orderId).toEqual(orderIdSummaryPage);
 
 });
